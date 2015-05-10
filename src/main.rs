892392838b43d12df034038cd76d3286be535ac0
akikan <h22s10akasaka@yahoo.co.jp>
2015-05-10 17:13:59 +0900

    made walls

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
         opengl,
         WindowSettings::new(
             "Tetris",
-            [20 * 10 +9, 20 * 20 +19]
+            [20 * 12 +11, 20 * 21 +20]
         )
         .exit_on_esc(true)
     );
@@ -28,7 +28,7 @@ fn main() {
 /*******************************************************/
     let mut array = minoGenerator::createMino('i');
     let mut test = board::Board::new();             //test space
-    test.state = test.marge(array[0], 2,2);
+    test.state = test.marge(array[0], 0,0);
 /******************************************************/      
 
     for e in window.events() {
@@ -37,20 +37,24 @@ fn main() {
             const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
             const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
             const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+            const GRAY:  [f32; 4] = [0.5, 0.5, 0.5, 1.0];
             gl.draw(r.viewport(), |c, gl|{
                 clear(BLACK, gl);
                 let cell = rectangle::square( 0 as f64,  0 as f64, 20 as f64);
                 
 
-                for y in 0..20{
-                    for x in 0..10{
+                for y in 2..23{
+                    for x in 0..12{
                         
-                        let transform = c.transform.trans((x*21) as f64,  (y*21) as f64);
-                        if  test.state[y][x] != 0{
-                            rectangle(GREEN, cell, transform, gl);
+                        let transform = c.transform.trans((x*21) as f64,  ((y-2)*21) as f64);
+                        if  test.state[y][x] == 9 {
+                            rectangle(GRAY, cell, transform, gl);
+                        }
+                        else if 0 == test.state[y][x]{
+                            rectangle(WHITE, cell, transform, gl);
                         }
                         else{
-                            rectangle(WHITE, cell, transform, gl);
+                            rectangle(GREEN, cell, transform, gl);
                         }
                     }
                 }
