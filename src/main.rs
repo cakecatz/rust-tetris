036c4db342869b1c6bd64dfafc111201a692e60b
akikan <h22s10akasaka@yahoo.co.jp>
2015-05-10 16:41:33 +0900

    changed this file

--- src/main.rs
+++ src/main.rs
@@ -8,37 +8,50 @@ use piston::event::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
+
+mod minoGenerator;
+mod board;
+
+
 fn main() {
     let opengl = OpenGL::_3_2;
     let window = Window::new(
         opengl,
         WindowSettings::new(
             "Tetris",
-            [20 * 10 +19, 20 * 20 +9]
+            [20 * 10 +9, 20 * 20 +19]
         )
         .exit_on_esc(true)
     );
 
-    let mut x = 0;
-    let mut y = 0; 
     let mut gl = GlGraphics::new(opengl);
-
+/*******************************************************/
+    let mut array = minoGenerator::createMino('i');
+    let mut test = board::Board::new();             //test space
+    test.state = test.marge(array[0], 2,2);
+/******************************************************/      
 
     for e in window.events() {
         if let Some(r) = e.render_args() {
             use graphics::*;
-            const GREEN: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-            const GRAY: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+            const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+            const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+            const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
             gl.draw(r.viewport(), |c, gl|{
-                clear(GREEN, gl);
+                clear(BLACK, gl);
                 let cell = rectangle::square( 0 as f64,  0 as f64, 20 as f64);
                 
 
-                for x in 0..10{
-                    for y in 0..20{
+                for y in 0..20{
+                    for x in 0..10{
                         
                         let transform = c.transform.trans((x*21) as f64,  (y*21) as f64);
-                        rectangle(GRAY, cell, transform, gl);
+                        if  test.state[y][x] != 0{
+                            rectangle(GREEN, cell, transform, gl);
+                        }
+                        else{
+                            rectangle(WHITE, cell, transform, gl);
+                        }
                     }
                 }
             });
